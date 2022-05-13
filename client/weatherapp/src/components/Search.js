c6e1132be23d012c871fb8ab8a0fd8900058b925
Berfin <berfinbaybars@hotmail.com>
2022-05-13 21:20:00 +0300

    Mapbox added

--- client/weatherapp/src/components/Search.js
+++ client/weatherapp/src/components/Search.js
@@ -1,21 +1,52 @@
-import React, { Fragment, useState } from 'react';
-import { connect } from 'react-redux'
+import React, { Fragment, useState, useEffect, useRef } from 'react';
+import { connect } from 'react-redux';
+import mapboxgl from 'mapbox-gl';
+import MapboxGeocoder from '@mapbox/mapbox-gl-geocoder';
 import { getWeather } from '../actions/weather';
+import Weather from './Weather';
+mapboxgl.accessToken = "pk.eyJ1IjoibXhibXhiIiwiYSI6ImNsMzRmcDVkajBoNTYza3BveHI1MnhrMTkifQ.OOqXLE5s0a4WcHQ-8KjASw"
 
 const Search = ({getWeather, weather: {weather, loading}}) => {
-    const [location, setLocation] = useState("");
-    
+
+    const mapContainer = useRef(null);
+    const map = useRef(null);
+    const [lng, setLng] = useState(35.25);
+    const [lat, setLat] = useState(39.19);
+    const zoom = 5;
+    const geocoder = new MapboxGeocoder({
+        accessToken: mapboxgl.accessToken,
+        mapboxgl: mapboxgl
+    });
+
+    useEffect(() => {
+        if (map.current) return;
+        map.current = new mapboxgl.Map({
+            container: mapContainer.current,
+            style: 'mapbox://styles/mapbox/streets-v11',
+            center: [lng, lat],
+            zoom: zoom
+        }).addControl(
+            geocoder
+        );
+        geocoder.on('result', (res) => {
+            setLng(res.result.geometry.coordinates[0]);
+            setLat(res.result.geometry.coordinates[1]);
+            let location = {
+                name: res.result.place_name_en,
+                lng: res.result.geometry.coordinates[0],
+                lat: res.result.geometry.coordinates[1]
+            }
+            getWeather(location)
+        })
+    });
+
     return (
         <div className="text-center container pt-5">
-            <div class="row justify-content-center">
-                <label for="location" class="col-2 col-form-label">Location</label>
-                <div class="col-4">
-                <input type="text" class="form-control" id="location" onChange={(e) => setLocation(e.target.value)} />
-                </div>
-                <button type="button" class="col-1 btn btn-secondary" onClick={() => getWeather(location)}>Search</button>
-            </div>
+            <div ref={mapContainer} className="map-container" />
+            <br />
+            <br />
             <Fragment>
-                {/* add new component for weather */}
+                {!loading && <Weather weather={weather} />}
             </Fragment>
         </div>
     )
