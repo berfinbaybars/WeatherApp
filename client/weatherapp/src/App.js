71d938307072050102c3e7371dd26a61f0f9ef20
Berfin <berfinbaybars@hotmail.com>
2022-05-13 21:19:28 +0300

    Router and pages added

--- client/weatherapp/src/App.js
+++ client/weatherapp/src/App.js
@@ -1,16 +1,21 @@
 import React, { Fragment } from 'react';
-import Search from './components/Search';
+import { Routes, Route } from 'react-router-dom';
 import store from './utils/store';
 import {Provider} from 'react-redux';
+import Landing from './pages/Landing';
+import Navbar from './components/layout/Navbar';
 
 const App = () => {
-  return (
-    <Provider store={store} >
-      <Fragment>
-        <Search />
-      </Fragment>
-    </Provider>
-  );
+    return (
+        <Provider store={store} >
+            <Fragment>
+                <Navbar />
+                <Routes>
+                    <Route path='/' element={<Landing />} />
+                </Routes>
+            </Fragment>
+        </Provider>
+    );
 }
 
 export default App;
