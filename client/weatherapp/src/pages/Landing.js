49e53205719d6142122d04d7c23890a8fd044387
Berfin <berfinbaybars@hotmail.com>
2022-05-13 21:20:26 +0300

    Landing page added

---
+++ client/weatherapp/src/pages/Landing.js
@@ -0,0 +1,12 @@
+import React, { Fragment } from 'react';
+import Search from '../components/Search';
+
+const Landing = () => {
+    return (
+        <Fragment>
+            <Search />
+        </Fragment>
+    )
+}
+
+export default Landing;
